044364bfadc572a24100680ebc25fa3e17e84ec3
acoppens-umd <acoppens@terpmail.umd.edu>
2025-05-11 20:02:22 -0400

    Fixed json exporter obstacle field

--- Utilities/Version.h
+++ Utilities/Version.h
@@ -5,7 +5,7 @@
 #define STRINGIZE(x) STRINGIZE_HELPER(x)
 #define WARNING(desc) message(__FILE__ "(" STRINGIZE(__LINE__) ") : Warning: " #desc)
 
-#define GIT_SHA1 "426d9b409b3f3586b4553486cd86068ee3e1164b"
+#define GIT_SHA1 "59723cde17e9958d2f31f18f05f989cfc54ad446"
 #define GIT_REFSPEC "refs/heads/main"
 #define GIT_LOCAL_STATUS "DIRTY"
 
